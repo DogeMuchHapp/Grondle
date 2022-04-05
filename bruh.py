6f424cf3525bdb97612899e2aa2d8e4a70c29c31
DogeMuchHapp <83430363+DogeMuchHapp@users.noreply.github.com>
2022-04-05 18:42:08 -0400

    yup

---
+++ bruh.py
@@ -0,0 +1,31 @@
+from colorama import init, Fore, Back
+init()
+word = "grond"
+loop = True
+while loop:
+    print(Back.WHITE + Fore.BLACK + "Start a new game? (y/quit)")
+    command = input()
+    if command == "quit":
+        loop = False
+    elif command == "y":
+        inner_loop = 0
+        print("Enter a word")
+        while inner_loop < 6:
+
+            attempt = input()
+
+            # Game logic
+            output = ""
+            for i in range(word.__len__()):
+                if attempt[i] == word[i]:
+                    output = output + Back.GREEN + attempt[i] + Back.RESET
+                elif attempt[i] in word:
+                    output = output + Back.YELLOW + attempt[i] + Back.RESET
+                else:
+                    output = output + attempt[i] + Back.RESET
+            print(output)
+            if word == attempt:
+                print(Fore.WHITE + "Congrats")
+                inner_loop = inner_loop + 6  # Reset game
+
+            inner_loop = inner_loop + 1
